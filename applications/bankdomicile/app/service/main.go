a3992d4b2194d5de3d72bc84fc57ee861c97b824
Aron Richter <aronrichter@gmail.com>
2024-06-18 11:36:14 -0300

    wip

--- applications/bankdomicile/app/service/main.go
+++ applications/bankdomicile/app/service/main.go
@@ -1,1 +1,14 @@
-package service
+package main
+
+import (
+	"fmt"
+
+	"github.com/aronrcihter/monorepo/applications/bankdomicile/gateway/usermetrics"
+	"github.com/aronrcihter/monorepo/libs/client"
+)
+
+func main() {
+	c := usermetrics.NewClient(client.Client{})
+
+	fmt.Println(c)
+}
